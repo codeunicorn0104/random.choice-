fac2734397aac31cd8a79eaf54fe6bc63a13db2f
S.Boopalan <breacher.darkworld@gmail.com>
2022-09-18 19:54:01 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,11 @@
+import random
+
+artical=["a","the"]
+things=["ball","bat","toy"]
+color=["red","pink","light blue"]
+
+a=random.choice(artical) #To pick random item in artical
+t=random.choice(things) #To pink random item in things
+c=random.choice(color) #To pick random item in color
+
+print (f"{a} {t} is {c} color" )  #here the fString is used
